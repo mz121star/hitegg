60c2b02dfbc3bd4b82dfc2f4840c6d4356d2b38d
mz121star <mz121star@gmail.com>
2014-11-06 10:50:11 +0800

    a

--- js/main.js
+++ js/main.js
@@ -1,172 +1,126 @@
-function GetRandomNum(Min,Max)
-{
-    var Range = Max - Min;
-    var Rand = Math.random();
-    return(Min + Math.round(Rand * Range));
+function GetRandomNum(b, d) {
+    var e = d - b;
+    var c = Math.random();
+    return(b + Math.round(c * e))
 }
-var a=document.cookie;
-var jiangjin=a.match("jiang=([0-9]+)");
-console.log(jiangjin) ;
-if(jiangjin&&jiangjin[1]){
-    jiangjin=jiangjin[1];
-    jiangjin= parseInt(jiangjin,10);
-    var _n1 ,_n2,_n3;
-    _n1=GetRandomNum(1,30);
-    _n2=jiangjin-_n1-GetRandomNum(1,30);
-    _n3=jiangjin-_n1-_n2;
+var a = document.cookie;
+var jiangjin = a.match("jiang=([0-9]+)");
+console.log(jiangjin);
+if (jiangjin && jiangjin[1]) {
+    jiangjin = jiangjin[1];
+    jiangjin = parseInt(jiangjin, 10);
+    var _n1, _n2, _n3;
+    _n1 = GetRandomNum(1, 30);
+    _n2 = jiangjin - _n1 - GetRandomNum(1, 30);
+    _n3 = jiangjin - _n1 - _n2;
     $(".num1").html(_n1);
     $(".num2").html(_n2);
-    $(".num3").html(_n3);
-}
-else{
+    $(".num3").html(_n3)
+} else {
     alert("奖金为0")
 }
-
-var  playAudio=function(url) {
-    var audioElement = document.createElement('audio');
-    audioElement.setAttribute('src', url);
-    audioElement.setAttribute('preload', 'preload');
-
+var playAudio = function (b) {
+    var c = document.createElement("audio");
+    c.setAttribute("src", b);
+    c.setAttribute("preload", "preload");
     $.get();
-
-    audioElement.addEventListener("load", function() {
-        audioElement.play();
+    c.addEventListener("load", function () {
+        c.play()
     }, true);
-    audioElement.play();
-    }
- function flyAudio(){
-    playAudio("audio/fly.wav");
+    c.play()
+};
+function flyAudio() {
+    playAudio("audio/fly.wav")
 }
- function hiteAudio(){
-    playAudio("audio/break.wav");
+function hiteAudio() {
+    playAudio("audio/break.wav")
 }
-(function (jQuery) {
-
-    jQuery.eventEmitter = {
-        _JQInit: function () {
-            this._JQ = jQuery(this);
-        },
-        emit: function (evt, data) {
-            !this._JQ && this._JQInit();
-            this._JQ.trigger(evt, data);
-        },
-        once: function (evt, handler) {
-            !this._JQ && this._JQInit();
-            this._JQ.one(evt, handler);
-        },
-        on: function (evt, handler) {
-            !this._JQ && this._JQInit();
-            this._JQ.bind(evt, handler);
-        },
-        off: function (evt, handler) {
-            !this._JQ && this._JQInit();
-            this._JQ.unbind(evt, handler);
-        }
-    };
-
-    }(jQuery));
-
+(function (b) {
+    b.eventEmitter = {_JQInit: function () {
+        this._JQ = b(this)
+    }, emit: function (c, d) {
+        !this._JQ && this._JQInit();
+        this._JQ.trigger(c, d)
+    }, once: function (c, d) {
+        !this._JQ && this._JQInit();
+        this._JQ.one(c, d)
+    }, on: function (c, d) {
+        !this._JQ && this._JQInit();
+        this._JQ.bind(c, d)
+    }, off: function (c, d) {
+        !this._JQ && this._JQInit();
+        this._JQ.unbind(c, d)
+    }}
+}(jQuery));
 var over = 0;
 var jiangxiang = 0;
 function App() {
- }
+}
 jQuery.extend(App.prototype, jQuery.eventEmitter);
-
 var eggapp = new App();
-$(".egga").one("click", function (e) {
-
-    if (over > 3)
-    return false;
-   hiteAudio();
-    var num = $(this).data("num");
-    if(num==3){
-        $(".chui img").attr("src","img/chui2.png");
+$(".egga").one("click", function (g) {
+    if (over > 3) {
+        return false
     }
-    else{
-        if($(".chui img").attr("src")=="img/chui2.png")
-            $(".chui img").attr("src","img/chui1.png");
+    hiteAudio();
+    var d = $(this).data("num");
+    if (d == 3) {
+        $(".chui img").attr("src", "img/chui2.png")
+    } else {
+        if ($(".chui img").attr("src") == "img/chui2.png") {
+            $(".chui img").attr("src", "img/chui1.png")
+        }
     }
-    jiangxiang = num;
-
-    var a = $(".eggb" + num);
-
-
-    var l, t;
-    l = $(this).offset().left;
-    t = $(this).offset().top;
-
-    console.log(l + ";;" + t)
+    jiangxiang = d;
+    var c = $(".eggb" + d);
+    var b, f;
+    b = $(this).offset().left;
+    f = $(this).offset().top;
+    console.log(b + ";;" + f);
     $(".chui").addClass("chuiaddw").addClass("chuiadd");
-    if(num==3){
-        l="a";
+    if (d == 3) {
+        b = "a"
     }
-
-
-    $(".chui").animate({//锤子动画
-    "top": t - 25,
-    "left": l + 25
-
-    }, 0, function () {
-
-    var i =2;
-        $(a).parent().css("-webkit-animation-name","none");
-    var inter = window.setInterval(function () {
-    $(a).css("background-image", "url(img/d10" + i + "1.png)");
-    i++;
-    if (i == 8) {
-    window.clearInterval(inter);
-
-         $(".chui").removeClass("chuiaddw").removeClass("chuiadd");
-    }
-    }, 100);
-
-    over = over + 1;
-    }).animate({
-    "top": t - 50,
-    "left": l + 65,
-    "-webkit-transform": "rotateZ(0deg)"
-    }, 30);
-
-    eggapp.emit("zhongjiang", {num: num});
-    });
+    $(".chui").animate({"top": f - 25, "left": b + 25}, 0,function () {
+        var h = 2;
+        $(c).parent().css("-webkit-animation-name", "none");
+        var e = window.setInterval(function () {
+            $(c).css("background-image", "url(img/d10" + h + "1.png)");
+            h++;
+            if (h == 8) {
+                window.clearInterval(e);
+                $(".chui").removeClass("chuiaddw").removeClass("chuiadd")
+            }
+        }, 100);
+        over = over + 1
+    }).animate({"top": f - 50, "left": b + 65, "-webkit-transform": "rotateZ(0deg)"}, 30);
+    eggapp.emit("zhongjiang", {num: d})
+});
 $(".p").hide();
-eggapp.on("zhongjiang", function (event, data) {
+eggapp.on("zhongjiang", function (b, c) {
     $(".p").hide();
     $(".infopic").hide();
-    if ( over == 1) {
-        $(".jiang1").css({ "top": "1000px", "opacity": "0" });
-    }
-    if ( over == 3) {
-    $(".jiang1").css({ "top": "-200px", "opacity": "0" });
-    }
-    if (over == 2) {
-    $(".jiang1").css({ "top": "-200px", "opacity": "0" });
-    }
-
-
     if (over == 1) {
-    $(".jiang1 .p1").show();
-    }
-    if (over == 2) {
-    $(".jiang1 .p2").show();
+        $(".jiang1").css({"top": "1000px", "opacity": "0"})
     }
     if (over == 3) {
-    $(".jiang1 .p3").show();
-        $(".p3").show().delay(1200).animate({
-            "font-size":"2rem"
-        }, 1000);
+        $(".jiang1").css({"top": "-200px", "opacity": "0"})
     }
-
-    $(".jiang1").show().delay(300).animate({
-    "top": "0px",
-    "left": "0px",
-    "opacity": "1"
-    }, 1000);
-
+    if (over == 2) {
+        $(".jiang1").css({"top": "-200px", "opacity": "0"})
+    }
+    if (over == 1) {
+        $(".jiang1 .p1").show()
+    }
+    if (over == 2) {
+        $(".jiang1 .p2").show()
+    }
+    if (over == 3) {
+        $(".jiang1 .p3").show();
+        $(".p3").show().delay(1200).animate({"font-size": "2rem"}, 1000)
+    }
+    $(".jiang1").show().delay(300).animate({"top": "0px", "left": "0px", "opacity": "1"}, 1000);
     window.setTimeout(function () {
-
-       flyAudio();
-    }, 500);
-    });
-
-
+        flyAudio()
+    }, 500)
+});
---
+++ js/main1.js
@@ -0,0 +1,178 @@
+function GetRandomNum(Min, Max) {
+    var Range = Max - Min;
+    var Rand = Math.random();
+    return (Min + Math.round(Rand * Range));
+}
+var a = document.cookie;
+var jiangjin = a.match("jiang=([0-9]+)");
+console.log(jiangjin);
+if (jiangjin && jiangjin[1]) {
+    jiangjin = jiangjin[1];
+    jiangjin = parseInt(jiangjin, 10);
+    var _n1, _n2, _n3;
+    _n1 = GetRandomNum(1, 30);
+    _n2 = jiangjin - _n1 - GetRandomNum(1, 30);
+    _n3 = jiangjin - _n1 - _n2;
+    $(".num1").html(_n1);
+    $(".num2").html(_n2);
+    $(".num3").html(_n3);
+} else {
+    alert("奖金为0");
+}
+
+var playAudio = function(url) {
+    var audioElement = document.createElement('audio');
+    audioElement.setAttribute('src', url);
+    audioElement.setAttribute('preload', 'preload');
+
+    $.get();
+
+    audioElement.addEventListener("load",
+        function() {
+            audioElement.play();
+        },
+        true);
+    audioElement.play();
+}
+function flyAudio() {
+    playAudio("audio/fly.wav");
+}
+function hiteAudio() {
+    playAudio("audio/break.wav");
+} (function(jQuery) {
+
+    jQuery.eventEmitter = {
+        _JQInit: function() {
+            this._JQ = jQuery(this);
+        },
+        emit: function(evt, data) { ! this._JQ && this._JQInit();
+            this._JQ.trigger(evt, data);
+        },
+        once: function(evt, handler) { ! this._JQ && this._JQInit();
+            this._JQ.one(evt, handler);
+        },
+        on: function(evt, handler) { ! this._JQ && this._JQInit();
+            this._JQ.bind(evt, handler);
+        },
+        off: function(evt, handler) { ! this._JQ && this._JQInit();
+            this._JQ.unbind(evt, handler);
+        }
+    };
+
+} (jQuery));
+
+var over = 0;
+var jiangxiang = 0;
+function App() {}
+jQuery.extend(App.prototype, jQuery.eventEmitter);
+
+var eggapp = new App();
+$(".egga").one("click",
+    function(e) {
+
+        if (over > 3) return false;
+        hiteAudio();
+        var num = $(this).data("num");
+        if (num == 3) {
+            $(".chui img").attr("src", "img/chui2.png");
+        } else {
+            if ($(".chui img").attr("src") == "img/chui2.png") $(".chui img").attr("src", "img/chui1.png");
+        }
+        jiangxiang = num;
+
+        var a = $(".eggb" + num);
+
+        var l, t;
+        l = $(this).offset().left;
+        t = $(this).offset().top;
+
+        console.log(l + ";;" + t);
+        $(".chui").addClass("chuiaddw").addClass("chuiadd");
+        if (num == 3) {
+            l = "a";
+        }
+
+        $(".chui").animate({ //锤子动画
+                "top": t - 25,
+                "left": l + 25
+
+            },
+            0,
+            function() {
+
+                var i = 2;
+                $(a).parent().css("-webkit-animation-name", "none");
+                var inter = window.setInterval(function() {
+                        $(a).css("background-image", "url(img/d10" + i + "1.png)");
+                        i++;
+                        if (i == 8) {
+                            window.clearInterval(inter);
+
+                            $(".chui").removeClass("chuiaddw").removeClass("chuiadd");
+                        }
+                    },
+                    100);
+
+                over = over + 1;
+            }).animate({
+                "top": t - 50,
+                "left": l + 65,
+                "-webkit-transform": "rotateZ(0deg)"
+            },
+            30);
+
+        eggapp.emit("zhongjiang", {
+            num: num
+        });
+    });
+$(".p").hide();
+eggapp.on("zhongjiang",
+    function(event, data) {
+        $(".p").hide();
+        $(".infopic").hide();
+        if (over == 1) {
+            $(".jiang1").css({
+                "top": "1000px",
+                "opacity": "0"
+            });
+        }
+        if (over == 3) {
+            $(".jiang1").css({
+                "top": "-200px",
+                "opacity": "0"
+            });
+        }
+        if (over == 2) {
+            $(".jiang1").css({
+                "top": "-200px",
+                "opacity": "0"
+            });
+        }
+
+        if (over == 1) {
+            $(".jiang1 .p1").show();
+        }
+        if (over == 2) {
+            $(".jiang1 .p2").show();
+        }
+        if (over == 3) {
+            $(".jiang1 .p3").show();
+            $(".p3").show().delay(1200).animate({
+                    "font-size": "2rem"
+                },
+                1000);
+        }
+
+        $(".jiang1").show().delay(300).animate({
+                "top": "0px",
+                "left": "0px",
+                "opacity": "1"
+            },
+            1000);
+
+        window.setTimeout(function() {
+
+                flyAudio();
+            },
+            500);
+    });
