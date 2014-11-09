71020b42a3c32e4ad163f9fc9c0965867f50a763
mz <mz121star@gmail.com>
2014-11-09 21:43:11 +0800

    a

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(function(){
-    $("#container").css({"width":$(window).width()*2-40 +"px" });
+    $("#container").css({"width":$(window).width()*2+50 +"px" });
 })
 
 function GetRandomNum(Min, Max) {
