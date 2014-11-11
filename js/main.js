2758e079e2ba479a149cbb43bcbd37bc4b47b007
mz121star <mz121star@gmail.com>
2014-11-11 17:16:56 +0800

    a

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ $(function(){
     $(".main-box").css({"width":$(window).width() +"px" });
    var w=($(window).width()-$("#mcover img").width())/2 ;
     $("#mcover img").css({"left":w})  ;
+    $(".main-box").css("top",($(window).height()-478)/3+"px")   ;
+    $("#shape").css("top",($(window).height()-478)/3+420+"px")    ;
 
 })
 
